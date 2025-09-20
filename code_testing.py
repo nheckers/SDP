08eb41d30f1a1017cca5fe0adeb48e9bebc6b398
Nicholas Heckers <nheckers2020@gmail.com>
2025-09-20 11:41:09 -0700

    Removed more unneeded files

--- code_testing.py
+++
@@ -1,9 +0,0 @@
-import cv2
-import numpy as np
-
-# Create a blank 300x300 black image
-blank_image = np.zeros((300, 300, 3), dtype=np.uint8)
-
-cv2.imshow("Blank Image", blank_image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
